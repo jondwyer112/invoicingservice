ac2a485968e9380bdcbee7f59bf22ac3be2e3dfd
Jonathan Dwyer <jondwyer@iu.edu>
2023-03-31 16:34:24 -0400

    first commit

---
+++ src/main/java/com/example/invoicingservice/InvoicingserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.invoicingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/invoicingservice/InvoicingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
